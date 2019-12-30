d8be1591732a7bbd478cbdecac401502100bc332
avazqueznj <alejandrovazquez@yahoo.com>
2019-12-30 11:41:44 -0800

    Fixing license inconsistency.

--- cmansion.ino/cmansion.ino
+++ cmansion.ino/cmansion.ino
@@ -1,14 +1,26 @@
 
 /***********************************************************************
  
- Chato's Mansion v1
- GNU GPL 3
- By Alejandro Vazquez and Alejandro Vazquez
- (c) 2019 Alejandro Vazquez
+    Chato's Mansion
+    Copyright (C) 2019  Alejandro Vazquez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  
  
  * Compile
-../arduino-cli.exe compile -b arduino:avr:leonardo ./P11N.ino
-arduino-cli.exe upload -p COM4 -b arduino:avr:leonardo P11.ino  
+ ./arduino-cli.exe compile -b arduino:avr:leonardo ./cmansion.ino
+ arduino-cli.exe upload -p COM4 -b arduino:avr:leonardo cmansion.ino
  
  ***********************************************************************/
 
